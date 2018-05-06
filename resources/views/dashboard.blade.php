722b5eabf6470c0c1a906ecc7cee387612e0c829
Dmitriy-Kulik <0663642996d@gmail.com>
2018-05-07 01:26:44 +0300

    1. Added modal windows with registration and authorization 2. Added creating users with dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h2>Sidebar</h2>
-                        <ul class="list-group">
-                            <li class="list-group-item active"><a href="{{route('dashboard.index')}}">Dashboard <span class="sr-only">(current)</span></a></li>
-                            <li class="list-group-item"><a href="{{route('charts.index')}}">Charts</a></li>
-                            <li class="list-group-item"><a href="{{route('tables.index')}}">Tables</a></li>
-                            <li class="list-group-item"><a href="{{route('cards.index')}}">Cards</a></li>
-                            <li class="list-group-item"><a href="{{route('map.index')}}">Map</a></li>
-                        </ul>
-                    </div>
-                </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2">
+                @include('components.sidebar')
             </div>
-            <div class="col-md-8">
+            <div class="col-sm-10">
                 <div class="card">
                     <div class="card-header">Dashboard</div>
-
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        You are logged in!
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <td>#</td>
+                                <td>Имя</td>
+                                <td>Фамилия</td>
+                                <td>Email</td>
+                                <td>Роль</td>
+                                <td>Дата создания</td>
+                                <td>Дата редактирования</td>
+                                <td>Дата удаления</td>
+                                <td>Редактировать</td>
+                                <td>Удалить</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $user)
+                                <tr>
+                                    <td>{{$user->id}}</td>
+                                    <td>{{$user->first_name}}</td>
+                                    <td>{{$user->last_name}}</td>
+                                    <td>{{$user->email}}</td>
+                                    <td>{{$user->role}}</td>
+                                    <td>{{$user->created_at}}</td>
+                                    <td>{{$user->updated_at}}</td>
+                                    <td>{{$user->deleted_at}}</td>
+                                    <td>
+                                        <a class="btn btn-default" href="{{route('is_super_admin.dashboard.edit', $user)}}"><i class="fa fa-edit"></i></a>
+                                    </td>
+                                    <td>
+                                        <form onsubmit="if(confirm('Удалить?')){ return true} else false" action="{{route('is_super_admin.dashboard.destroy', $user)}}" method="post">
+                                            <input type="hidden" name="_method" value="DELETE">
+                                            <button type="submit" class="btn"><i class="fa fa-trash"></i></button>
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div class="text-center">
+                            {{ $users -> links() }}
+                        </div>
+                        <div class="text-center">
+                            <a class="btn btn-primary" href="{{route('is_super_admin.dashboard.create')}}">Add user</a>
+                        </div>
                     </div>
                 </div>
             </div>
